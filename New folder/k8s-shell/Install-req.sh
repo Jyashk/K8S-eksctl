c4af46b88acebf68d02944367723ebc7aa23baed
jupalli yashwanth kumar <yashwanthhny@gmail.com>
2024-07-17 06:50:16 +0530

    added shell script

---
+++ New folder/k8s-shell/Install-req.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+USER_ID=$(id -u)
+
+if [$USER_ID -ne 0]
+then 
+    echo "Please switch to root user to perform this activity"
+    exit 1
+else 
+    echo " Youre a super user" 
+fi
+
+#Installing Kubectl tool
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+#Install Aws Cli tool 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+
+#Install EKS CTL 
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+echo "eksctl version"
+
+
